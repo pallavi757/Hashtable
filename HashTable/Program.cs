2430357fecb88ea0706c8abd506d82c9bbda1bff
unknown <kinekarpallavi21@gmail.com>
2022-03-30 19:26:21 +0530

    Find Frquency of words from sentense

---
+++ HashTable/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+namespace HashTable
+{
+    class Program
+    {
+        public static void count_freq(string msg, MyMapNode<string, int> storedDictionary)
+        {
+            string[] words = msg.Split(' ');
+            foreach (string word in words)
+            {
+                if (storedDictionary.Exists(word))//Check if word already exist in dictionary 
+                {
+                    storedDictionary.Add(word, storedDictionary.Get(word) +1);
+                }
+                else
+                {
+                    storedDictionary.Add(word,1);
+                }
+            }
+            storedDictionary.Display();
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Hash table program");
+            MyMapNode<string, int> storedDictionary = new MyMapNode<string, int>(5);
+            count_freq("To be or not to be", storedDictionary);
+
+        }
+    }
+}
